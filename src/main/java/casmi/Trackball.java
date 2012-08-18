c9c6b02f9ba785447ad57304831290a33f9c8954
Toshiki TAKEUCHI <take@xcoo.jp>
2012-08-18 15:52:03 +0900

    Move Trackball extension to main

--- src/main/java/casmi/Trackball.java
+++ src/main/java/casmi/Trackball.java
@@ -17,10 +17,9 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package casmi.extension.trackball;
+package casmi;
 
 import casmi.graphics.object.GraphicsObject;
-
 
 /**
  * Class for Trackball manipulation.
@@ -60,6 +59,10 @@ public class Trackball {
 	    
 	    this.width  = width;
 	    this.height = height;
+	}
+	
+	public Trackball(Applet applet) {
+	    this(applet.getWidth(), applet.getHeight());
 	}
 	
 	public void update(int mouseX, int mouseY, int prvMouseX, int prvMouseY) {
@@ -318,23 +321,23 @@ public class Trackball {
 	private final double[] multMatrix(final double[] mat1, final double[] mat2) {
 	    double[] ret = new double[16];
 	    
-	    ret[0]  = mat1[0] * mat2[0] + mat1[1] * mat2[4] + mat1[2] * mat2[8] + mat1[3] * mat2[12];
-	    ret[1]  = mat1[0] * mat2[1] + mat1[1] * mat2[5] + mat1[2] * mat2[9] + mat1[3] * mat2[13];
-	    ret[2]  = mat1[0] * mat2[2] + mat1[1] * mat2[6] + mat1[2] * mat2[10] + mat1[3] * mat2[14];
-	    ret[3]  = mat1[0] * mat2[3] + mat1[1] * mat2[7] + mat1[2] * mat2[11] + mat1[3] * mat2[15];
+	    ret[0]  = mat1[0]  * mat2[0] + mat1[1]  * mat2[4] + mat1[2]  * mat2[8]  + mat1[3]  * mat2[12];
+	    ret[1]  = mat1[0]  * mat2[1] + mat1[1]  * mat2[5] + mat1[2]  * mat2[9]  + mat1[3]  * mat2[13];
+	    ret[2]  = mat1[0]  * mat2[2] + mat1[1]  * mat2[6] + mat1[2]  * mat2[10] + mat1[3]  * mat2[14];
+	    ret[3]  = mat1[0]  * mat2[3] + mat1[1]  * mat2[7] + mat1[2]  * mat2[11] + mat1[3]  * mat2[15];
 	    
-	    ret[4]  = mat1[4] * mat2[0] + mat1[5] * mat2[4] + mat1[6] * mat2[8] + mat1[7] * mat2[12];
-	    ret[5]  = mat1[4] * mat2[1] + mat1[5] * mat2[5] + mat1[6] * mat2[9] + mat1[7] * mat2[13];
-	    ret[6]  = mat1[4] * mat2[2] + mat1[5] * mat2[6] + mat1[6] * mat2[10] + mat1[7] * mat2[14];
-	    ret[7]  = mat1[4] * mat2[3] + mat1[5] * mat2[7] + mat1[6] * mat2[11] + mat1[7] * mat2[15];
+	    ret[4]  = mat1[4]  * mat2[0] + mat1[5]  * mat2[4] + mat1[6]  * mat2[8]  + mat1[7]  * mat2[12];
+	    ret[5]  = mat1[4]  * mat2[1] + mat1[5]  * mat2[5] + mat1[6]  * mat2[9]  + mat1[7]  * mat2[13];
+	    ret[6]  = mat1[4]  * mat2[2] + mat1[5]  * mat2[6] + mat1[6]  * mat2[10] + mat1[7]  * mat2[14];
+	    ret[7]  = mat1[4]  * mat2[3] + mat1[5]  * mat2[7] + mat1[6]  * mat2[11] + mat1[7]  * mat2[15];
 	    
-	    ret[8]  = mat1[8] * mat2[0] + mat1[9] * mat2[4] + mat1[10] * mat2[8] + mat1[11] * mat2[12];
-	    ret[9]  = mat1[8] * mat2[1] + mat1[9] * mat2[5] + mat1[10] * mat2[9] + mat1[11] * mat2[13];
-	    ret[10] = mat1[8] * mat2[2] + mat1[9] * mat2[6] + mat1[10] * mat2[10] + mat1[11] * mat2[14];
-	    ret[11] = mat1[8] * mat2[3] + mat1[9] * mat2[7] + mat1[10] * mat2[11] + mat1[11] * mat2[15];
+	    ret[8]  = mat1[8]  * mat2[0] + mat1[9]  * mat2[4] + mat1[10] * mat2[8]  + mat1[11] * mat2[12];
+	    ret[9]  = mat1[8]  * mat2[1] + mat1[9]  * mat2[5] + mat1[10] * mat2[9]  + mat1[11] * mat2[13];
+	    ret[10] = mat1[8]  * mat2[2] + mat1[9]  * mat2[6] + mat1[10] * mat2[10] + mat1[11] * mat2[14];
+	    ret[11] = mat1[8]  * mat2[3] + mat1[9]  * mat2[7] + mat1[10] * mat2[11] + mat1[11] * mat2[15];
 	    
-	    ret[12] = mat1[12] * mat2[0] + mat1[13] * mat2[4] + mat1[14] * mat2[8] + mat1[15] * mat2[12];
-	    ret[13] = mat1[12] * mat2[1] + mat1[13] * mat2[5] + mat1[14] * mat2[9] + mat1[15] * mat2[13];
+	    ret[12] = mat1[12] * mat2[0] + mat1[13] * mat2[4] + mat1[14] * mat2[8]  + mat1[15] * mat2[12];
+	    ret[13] = mat1[12] * mat2[1] + mat1[13] * mat2[5] + mat1[14] * mat2[9]  + mat1[15] * mat2[13];
 	    ret[14] = mat1[12] * mat2[2] + mat1[13] * mat2[6] + mat1[14] * mat2[10] + mat1[15] * mat2[14];
 	    ret[15] = mat1[12] * mat2[3] + mat1[13] * mat2[7] + mat1[14] * mat2[11] + mat1[15] * mat2[15];
 	        
