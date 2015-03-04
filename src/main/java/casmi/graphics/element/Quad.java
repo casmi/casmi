90f6514a9cbce0685186e7a92beb69e22a3b11c4
BOXP <tiyotiyouda@gmail.com>
2015-03-04 14:34:41 +0900

    Fix typo

--- src/main/java/casmi/graphics/element/Quad.java
+++ src/main/java/casmi/graphics/element/Quad.java
@@ -115,7 +115,7 @@ public class Quad extends Element {
         this.x2 = x2;
         this.y2 = y2;
         this.x3 = x3;
-        this.y3 = x3;
+        this.y3 = y3;
         this.x4 = x4;
         this.y4 = y4;
         calcG();
